eb8a6550c9f61b958e0692aaaac80f7fe79fd7e6
Uz0r0 <uzar.keneshbekov@gmail.com>
2026-09-25 22:50:25 +0600

    Two Sum (day3)

---
+++ day3.py
@@ -0,0 +1,9 @@
+# 1 Two Sum 
+# Runtime: 2151ms
+# Memory: 13.39MB
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return list[i, j]
